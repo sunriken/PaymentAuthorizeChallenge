e9b86a9afdfbc81484152995243893e61461389c
Carlos Diaz Maya <sunriken@gmail.com>
2023-04-04 00:39:25 -0500

    Start of Unit Test creation.

--- src/test/java/com/adidas/pac/PaymentAuthorizeChallengeMainTest.java
+++ src/test/java/com/adidas/pac/PaymentAuthorizeChallengeMainTest.java
@@ -26,4 +26,25 @@ public class PaymentAuthorizeChallengeMainTest {
 
     assertTrue(Boolean.TRUE);
   }
+
+  @Test
+  public void testMainExample1() {
+    String data =
+        "{\"payment-session\": {\"payment-id\": 89087653, \"cc\": \"visa\", \"amount\": 120, \"time\": \"2022-02-13T10:00:00.000Z\"}}\r\n"
+            + "{\"payment-session\": {\"payment-id\": 89087653}}\r\n"
+            + "{\"payment-rules\": {\"max-limit\": 100}}\r\n";
+
+    InputStream stdin = System.in;
+    try {
+      System.setIn(new ByteArrayInputStream(data.getBytes()));
+      assertDoesNotThrow(
+          () -> {
+            PaymentAuthorizeChallengeMain.main(new String[0]);
+          });
+    } finally {
+      System.setIn(stdin);
+    }
+
+    assertTrue(Boolean.TRUE);
+  }
 }
